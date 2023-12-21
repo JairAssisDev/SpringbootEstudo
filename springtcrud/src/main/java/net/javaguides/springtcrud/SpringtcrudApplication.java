5a28c74cf3ab25804166baaf3f275a768463c7d4
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2023-12-20 23:09:55 -0300

    Add files via upload

---
+++ springtcrud/src/main/java/net/javaguides/springtcrud/SpringtcrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springtcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtcrudApplication.class, args);
+	}
+
+}
---
+++ springtcrud/src/test/java/net/javaguides/springtcrud/SpringtcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springtcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
